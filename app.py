4b4be00fafdf0f560bb800d4cde9c66c3fd828df
IgnacioLanderos <ilanderos@uc.cl>
2024-06-23 01:10:26 -0400

    DB

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import json
 import base64
 
 # Create SQLAlchemy instance
@@ -23,37 +24,45 @@ def create_app():
         def __repr__(self):
             return f'<Message {self.data}>'
 
-    # Create the database tables
+        def to_dict(self):
+            return json.loads(self.data)  # Convertir el JSON almacenado en el campo 'data' en un diccionario Python
+
+    # Create the database tables and delete all existing data
     with app.app_context():
+        # Eliminar todos los registros de la tabla Message
+        db.session.query(Message).delete()
+        db.session.commit()
+
+        # Crear las tablas en la base de datos
         db.create_all()
 
     # Route to handle Pub/Sub push messages
     @app.route('/pubsub/push', methods=['POST'])
     def pubsub_push_handler():
         try:
-            # Obtain the message from the request body
+            # Obtener el mensaje del cuerpo de la solicitud
             envelope = request.get_json()
             if not envelope:
                 return 'Bad Request: no Pub/Sub message received', 400
 
-            # Pub/Sub message
+            # Mensaje de Pub/Sub
             pubsub_message = envelope.get('message')
             if not pubsub_message:
                 return 'Bad Request: no Pub/Sub message received', 400
 
-            # Get the message data
+            # Obtener los datos del mensaje
             message_data = pubsub_message.get('data')
             if message_data:
-                # Decode base64 message
+                # Decodificar el mensaje base64
                 decoded_message = base64.b64decode(message_data).decode('utf-8')
                 print(f'Received message: {decoded_message}')
 
-                # Save message to database
+                # Guardar el mensaje en la base de datos
                 message = Message(data=decoded_message)
                 db.session.add(message)
                 db.session.commit()
 
-                # Confirm message received
+                # Confirmar la recepción del mensaje
                 return jsonify(status='Message received'), 200
             else:
                 print('No data in message')
@@ -66,6 +75,7 @@ def create_app():
     @app.route('/messages', methods=['GET'])
     def get_messages():
         messages = Message.query.all()
+        # Convertir cada objeto Message en un diccionario usando el método to_dict()
         message_dicts = [message.to_dict() for message in messages]
         return jsonify(message_dicts), 200
 
